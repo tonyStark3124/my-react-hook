0e8d87a10ede36063e3df95f916b01ec3633ccda
Your Name <you@example.com>
2024-08-06 17:33:47 +0300

    םם

--- my-react-app-in/src/App.js
+++ my-react-app-in/src/App.js
@@ -1,16 +1,17 @@
 
 import './App.css';
-import App3 from './App3';
-import App4 from './App4';
+// import App3 from './App3';
+// import App4 from './App4';
 
 
 function App() {
+
   return (
      <div className='App'>
+     {/* <App3/>
+     <App4/> */}
 
-     <App3/>
 
-     <App4/>
 
     </div>
   );
