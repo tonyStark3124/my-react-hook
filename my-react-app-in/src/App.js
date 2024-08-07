5655a00d016e68f1b637fd4f9d91a757f88512d8
Your Name <you@example.com>
2024-08-07 14:22:00 +0300

    po

--- my-react-app-in/src/App.js
+++ my-react-app-in/src/App.js
@@ -1,28 +1,21 @@
 
 import './App.css';
-<<<<<<< HEAD
 // import App3 from './App3';
 // import App4 from './App4';
-=======
-import App3 from './App3';
-import App4 from './App4';
 import AppProject from './AppProject';
->>>>>>> 662071604a42df3320c63f96623730d12dc574cc
+
 
 
 function App() {
-
   return (
      <div className='App'>
-<<<<<<< HEAD
+
      {/* <App3/>
      <App4/> */}
 
 
-=======
-      <AppProject/>
->>>>>>> 662071604a42df3320c63f96623730d12dc574cc
 
+      <AppProject/>
     </div>
   );
 }
